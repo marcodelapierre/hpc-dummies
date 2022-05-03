dd64106f880fc8e6f508062f5d67741926c52782
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-03 10:49:58 +0800

    created include/ dir for my_timer and more

--- saxpy/saxpy_simd.cpp
+++ saxpy/saxpy_simd.cpp
@@ -1,32 +1,10 @@
 #include <iostream>
 #include <cmath>
-#include <chrono>
 #include <ctime>
+#include "my_timer.h"
 
 using namespace std;
 
-
-// Timer class
-// from https://github.com/bennylp/saxpy-benchmark
-class my_timer
-{
-public:
-    my_timer() { reset(); }
-    void reset() {
-      t0_ = std::chrono::high_resolution_clock::now();
-    }
-    double elapsed(bool reset_timer=false) {
-      std::chrono::high_resolution_clock::time_point t =
-        std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> time_span =
-        std::chrono::duration_cast<std::chrono::duration<double>>(t - t0_);
-      if (reset_timer)
-        reset();
-      return time_span.count();
-    }
-private:
-    std::chrono::high_resolution_clock::time_point t0_;
-};
 
 // Verify SAXPY result
 float verify_saxpy( const float tot, const size_t n, const float* const y )

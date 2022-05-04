a348723077374b34ee219b31cd1bf879381e27be
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-04 17:03:11 +0800

    saxpy: kokkos (from kokkos-openmptarget-examples)

---
+++ saxpy/build_saxpy_kokkos.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+target="topaz-cpu"
+binary_name="saxpy_kokkos"
+
+if [ $target == "topaz-gpu" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
+  module load cmake/3.18.0
+  module load openmpi-ucx-gpu/4.0.2
+elif [ $target == "topaz-cpu" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
+  module load cmake/3.18.0
+  module load openmpi-ucx/4.0.2
+elif [ $target == "zeus" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
+  module load cmake/3.18.0
+  module swap sandybridge broadwell
+  module swap gcc gcc/8.3.0
+  module load openmpi/2.1.2
+else
+  echo "Wrong target, exiting."
+  exit 1
+fi
+
+rm -f ${binary_name}*.x
+rm -rf build && mkdir build && cd build
+
+cmake .. \
+  -DCMAKE_CXX_COMPILER=mpicxx \
+  -DKokkos_ROOT="$kokkos_dir" \
+  -DKokkos_DIR="$kokkos_dir/lib64/cmake/Kokkos"
+
+make
+cp -p ${binary_name}*.x ..
+cd ..
+
---
+++ saxpy/saxpy_kokkos.cpp
@@ -0,0 +1,103 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 3.0
+//       Copyright (2020) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <Kokkos_Core.hpp>
+#include <cmath>
+
+// float type: float (saxpy) or double (daxpy)
+#define FLOAT_TYPE float
+
+
+struct AXPBY {
+  using view_t = Kokkos::View<FLOAT_TYPE *>;
+  int N;
+  view_t x, y, z;
+  bool fence_all;
+
+  AXPBY(int N_, bool fence_all_)
+      : N(N_), x(view_t("X", N)), y(view_t("Y", N)), z(view_t("Z", N)),
+        fence_all(fence_all_) {}
+
+  KOKKOS_FUNCTION
+  void operator()(int i) const { z(i) = x(i) + y(i); }
+
+  double kk_axpby(int R) {
+    // Warmup
+    Kokkos::parallel_for("kk_axpby_wup", N, *this);
+    Kokkos::fence();
+
+    Kokkos::Timer timer;
+    for (int r = 0; r < R; r++) {
+      Kokkos::parallel_for("kk_axpby", N, *this);
+      if (fence_all)
+        Kokkos::fence();
+    }
+    Kokkos::fence();
+    double time = timer.seconds();
+    return time;
+  }
+
+  void run_test(int R) {
+    double bytes_moved = 1. * sizeof(FLOAT_TYPE) * N * 3 * R;
+    double GB = bytes_moved / 1024 / 1024 / 1024;
+    double time_kk = kk_axpby(R);
+    printf("AXPBY KK: %e s %e GB/s\n", time_kk, GB / time_kk);
+  }
+};
+
+
+int main(int argc, char *argv[]) {
+
+Kokkos::initialize(argc, argv);
+{
+
+int N = argc > 1 ? atoi(argv[1]) : 1000000;
+int R = argc > 2 ? atoi(argv[2]) : 10;
+AXPBY axpby(N, false);
+axpby.run_test(R);
+
+}
+Kokkos::finalize();
+return 0;
+}

e7f24e559fdc6e5d4af3991dfab717da57f0e2e3
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-04 16:47:31 +0800

    hello: update to kokkos build

--- hello/build_hello_kokkos.sh
+++ hello/build_hello_kokkos.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-module load cmake/3.18.0
-# Topaz GPU
-kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
-module load openmpi-ucx-gpu/4.0.2
-# Topaz CPU
-#kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
-#module load openmpi-ucx/4.0.2
-# Zeus
-#kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
-#module swap sandybridge broadwell
-#module swap gcc gcc/8.3.0
-#module load openmpi/2.1.2
+target="topaz-cpu"
+binary_name="hello_kokkos"
 
-rm -f hello_kokkos*.x
+if [ $target == "topaz-gpu" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
+  module load cmake/3.18.0
+  module load openmpi-ucx-gpu/4.0.2
+elif [ $target == "topaz-cpu" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
+  module load cmake/3.18.0
+  module load openmpi-ucx/4.0.2
+elif [ $target == "zeus" ] ; then
+  kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
+  module load cmake/3.18.0
+  module swap sandybridge broadwell
+  module swap gcc gcc/8.3.0
+  module load openmpi/2.1.2
+else
+  echo "Wrong target, exiting."
+  exit 1
+fi
+
+rm -f ${binary_name}*.x
 rm -rf build && mkdir build && cd build
 
 cmake .. \
@@ -22,6 +31,6 @@ cmake .. \
   -DKokkos_DIR="$kokkos_dir/lib64/cmake/Kokkos"
 
 make
-cp -p hello_kokkos*.x ..
+cp -p ${binary_name}*.x ..
 cd ..
 

2f07c2b07d0b6952c0bede429baec1757fed8cc3
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-04 16:33:40 +0800

    hello: updated kokkos build

--- hello/build_hello_kokkos.sh
+++ hello/build_hello_kokkos.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Topaz
-#kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
-#module load cmake/3.18.0
-#module load openmpi-ucx-gpu/4.0.2
-
-# Zeus
-kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
-module swap sandybridge broadwell
 module load cmake/3.18.0
-module swap gcc gcc/8.3.0
-module load openmpi/2.1.2
+# Topaz GPU
+kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
+module load openmpi-ucx-gpu/4.0.2
+# Topaz CPU
+#kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
+#module load openmpi-ucx/4.0.2
+# Zeus
+#kokkos_dir="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
+#module swap sandybridge broadwell
+#module swap gcc gcc/8.3.0
+#module load openmpi/2.1.2
 
 rm -f hello_kokkos*.x
 rm -rf build && mkdir build && cd build

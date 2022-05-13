8e432b6e67227b423acd24264670af9b5e42d981
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-13 15:46:56 +0800

    saxpy: added examples for profiling with caliper and kokkos tools

--- saxpy/build_saxpy_kokkos.sh
+++ saxpy/build_saxpy_kokkos.sh
@@ -10,13 +10,16 @@ binary_name="saxpy_kokkos"
 
 if [ $target == "topaz-gpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
+  Caliper_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/caliper/apps"
   module load cmake/3.18.0
-  module load cuda/11.1
+  module load cuda/11.4.2
 elif [ $target == "topaz-cpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
+  Caliper_ROOT=""
   module load cmake/3.18.0
 elif [ $target == "zeus" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
+  Caliper_ROOT=""
   module load cmake/3.18.0
   module swap sandybridge broadwell
   module swap gcc gcc/8.3.0
@@ -38,7 +41,8 @@ cmake .. \
 `#  -DCMAKE_CXX_COMPILER="g++"` \
   -DCMAKE_CXX_FLAGS="${cmake_cxx_flags}" \
   -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
-  -DCMAKE_PREFIX_PATH="${Kokkos_ROOT}" #\
+  -DCMAKE_PREFIX_PATH="${Kokkos_ROOT}" \
+  -DCaliper_DIR="${Caliper_ROOT}" #\
 #  -DKokkos_DIR="$Kokkos_ROOT/lib64/cmake/Kokkos"
 
 make ${verbose_make_string}

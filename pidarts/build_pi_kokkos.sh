7ff04ca86244b067d714f7b9bc37fe68a1a4e788
Marco De La Pierre <marco.delapierre@gmail.com>
2022-08-25 21:27:30 +0800

    pidarts kokkos: updated build script

--- pidarts/build_pi_kokkos.sh
+++ pidarts/build_pi_kokkos.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 target="topaz-cpu"
-binary_name="pi_kokkos"
+verbose_make="0"
 cmake_cxx="g++"
+cmake_cxx_flags="-g -O3"
+cmake_build_type="Release"
+#cmake_build_type="RelWithDebInfo"
+
+binary_name="pi_kokkos"
 
 if [ $target == "topaz-gpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
@@ -24,15 +29,23 @@ else
   exit 1
 fi
 
+if [ $verbose_make != "0" ] ; then
+  verbose_make_string="VERBOSE=1"
+else
+  verbose_make_string=""
+fi
+
 rm -f ${binary_name}*.x
 rm -rf build && mkdir build && cd build
 
 cmake .. \
   -DCMAKE_CXX_COMPILER="$cmake_cxx" \
+  -DCMAKE_CXX_FLAGS="${cmake_cxx_flags}" \
+  -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
   -DCMAKE_PREFIX_PATH="$Kokkos_ROOT" #\
 #  -DKokkos_ROOT="$kokkos_dir/lib64/cmake/Kokkos"
 
-make
+make ${verbose_make_string}
 cp -p ${binary_name}*.x ..
 cd ..
 rm -rf build

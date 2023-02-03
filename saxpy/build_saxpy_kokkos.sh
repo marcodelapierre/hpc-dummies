67d5d1804815a66cbd8291e7f070b9dc1fb56f91
Marco De La Pierre <marco.delapierre@gmail.com>
2023-02-03 21:26:20 +0800

    saxpy: not building kkblas for gpus for now
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- saxpy/build_saxpy_kokkos.sh
+++ saxpy/build_saxpy_kokkos.sh
@@ -11,18 +11,19 @@ binary_name="saxpy_kokkos"
 
 if [ $target == "topaz-gpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
-  Caliper_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/caliper/apps"
-  Kernels_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-kernels/apps"
+  #Caliper_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/caliper/apps"
+  #does not build, not sure why (undefined references to cublas functions)
+  ###Kernels_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-kernels/apps"
   module load cmake/3.18.0
   module load cuda/11.4.2
 elif [ $target == "topaz-cpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
-  Caliper_ROOT=""
+  #Caliper_ROOT=""
   Kernels_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-kernels-cpu/apps"
   module load cmake/3.18.0
 elif [ $target == "mulan-gpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-mulan/apps"
-  Caliper_ROOT=""
+  #Caliper_ROOT=""
   Kernels_ROOT=""
   cmake_cxx="hipcc"
   . /pawsey/mulan/bin/init-cmake-3.21.4.sh
@@ -31,13 +32,13 @@ elif [ $target == "mulan-gpu" ] ; then
   module load rocm/4.5.0
 elif [ $target == "mulan-cpu" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-mulan-cpu/apps"
-  Caliper_ROOT=""
+  #Caliper_ROOT=""
   Kernels_ROOT=""
   . /pawsey/mulan/bin/init-cmake-3.21.4.sh
   module unload gcc/9.3.0
 elif [ $target == "zeus" ] ; then
   Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
-  Caliper_ROOT=""
+  #Caliper_ROOT=""
   Kernels_ROOT=""
   module load cmake/3.18.0
   module swap sandybridge broadwell
@@ -64,8 +65,8 @@ cmake .. \
   -DCMAKE_CXX_FLAGS="${cmake_cxx_flags}" \
   -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
   -DCMAKE_PREFIX_PATH="${Kokkos_ROOT}" \
-  -DCaliper_DIR="${Caliper_ROOT}" \
   -DKokkosKernels_ROOT="${Kernels_ROOT}" #\
+#  -DCaliper_DIR="${Caliper_ROOT}" #\
 #  -DKokkos_DIR="$Kokkos_ROOT/lib64/cmake/Kokkos"
 
 make ${verbose_make_string}

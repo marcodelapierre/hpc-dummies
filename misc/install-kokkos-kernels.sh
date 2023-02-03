cb4a236ce800162aa6aea5bd4aa91b71a76d63e3
Marco De La Pierre <marco.delapierre@gmail.com>
2023-02-03 20:19:16 +0800

    misc: added install kokkos kernels for topaz gpu
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ misc/install-kokkos-kernels.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#SBATCH --account=pawsey0001
+#SBATCH --partition=gpuq-dev
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=00:20:00
+#SBATCH --output=%x.out
+
+#echo $SLURM_JOB_NAME
+
+download_kernels="0"
+kokkos_version="3.5.00"
+
+module load cuda/11.4.2
+module load cmake/3.18.0
+
+if [ "$download_kernels" != "0" ] ; then
+ rm -rf kokkos-kernels
+ git clone git@github.com:kokkos/kokkos-kernels
+fi
+
+cd kokkos-kernels
+git checkout $kokkos_version
+
+mkdir build
+cd build
+
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/../apps \
+  -DCMAKE_CXX_COMPILER=$(pwd)/../bin/nvcc_wrapper \
+  -DKokkos_ROOT=$(pwd)/../../kokkos
+
+make -j $SLURM_CPUS_PER_TASK
+sg pawsey0001 -c 'make install'
+

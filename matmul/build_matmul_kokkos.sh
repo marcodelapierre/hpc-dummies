803b114a7d387fd5a5194eeaab7c29023f6c91ee
Marco De La Pierre <marco.delapierre@gmail.com>
2023-02-03 18:21:54 +0800

    matmul: added kokkos
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ matmul/build_matmul_kokkos.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+target="${target:-topaz-gpu}"
+verbose_make="0"
+cmake_cxx="g++"
+cmake_cxx_flags="-g"
+cmake_build_type="Release"
+#cmake_build_type="RelWithDebInfo"
+
+binary_name="matmul_kokkos"
+
+if [ $target == "topaz-gpu" ] ; then
+  Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos/apps"
+  Caliper_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/caliper/apps"
+  module load cmake/3.18.0
+  module load cuda/11.4.2
+elif [ $target == "topaz-cpu" ] ; then
+  Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-cpu/apps"
+  Caliper_ROOT=""
+  module load cmake/3.18.0
+elif [ $target == "mulan-gpu" ] ; then
+  Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-mulan/apps"
+  Caliper_ROOT=""
+  cmake_cxx="hipcc"
+  . /pawsey/mulan/bin/init-cmake-3.21.4.sh
+  module unload gcc/9.3.0
+  module load craype-accel-amd-gfx908
+  module load rocm/4.5.0
+elif [ $target == "mulan-cpu" ] ; then
+  Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-mulan-cpu/apps"
+  Caliper_ROOT=""
+  . /pawsey/mulan/bin/init-cmake-3.21.4.sh
+  module unload gcc/9.3.0
+elif [ $target == "zeus" ] ; then
+  Kokkos_ROOT="/group/pawsey0001/mdelapierre/VISCOUS/kokkos-setup/kokkos-zeus/apps"
+  Caliper_ROOT=""
+  module load cmake/3.18.0
+  module swap sandybridge broadwell
+  module swap gcc gcc/8.3.0
+elif [ $target == "mac-cpu" ] ; then
+  Kokkos_ROOT="$HOME/software/kokkos/apps"
+  cmake_cxx="g++-12"
+else
+  echo "Wrong target, exiting."
+  exit 1
+fi
+
+if [ $verbose_make != "0" ] ; then
+  verbose_make_string="VERBOSE=1"
+else
+  verbose_make_string=""
+fi
+
+rm -f ${binary_name}*.x
+rm -rf build && mkdir build && cd build
+
+cmake .. \
+  -DCMAKE_CXX_COMPILER="${cmake_cxx}" \
+  -DCMAKE_CXX_FLAGS="${cmake_cxx_flags}" \
+  -DCMAKE_BUILD_TYPE="${cmake_build_type}" \
+  -DCMAKE_PREFIX_PATH="${Kokkos_ROOT}" \
+  -DCaliper_DIR="${Caliper_ROOT}" #\
+#  -DKokkos_DIR="$Kokkos_ROOT/lib64/cmake/Kokkos"
+
+make ${verbose_make_string}
+cp -p ${binary_name}*.x ..
+cd ..
+rm -rf build
---
+++ matmul/matmul_kokkos.cpp
@@ -0,0 +1,85 @@
+#include <Kokkos_Core.hpp>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+// Kokkos implementation using lambda functions
+
+int main( int argc, char** argv ) {
+
+Kokkos::initialize(argc, argv);
+{
+
+// Size of problem
+const int N = 500;
+//const int N2 = N * N;
+// Random values
+const float AVAL = 30.;
+const float BVAL = 40.;
+const float tot = AVAL * BVAL * N;
+// Typdefs
+typedef Kokkos::View<float*>   ViewVectorType;
+typedef Kokkos::View<float**>  ViewMatrixType;
+typedef Kokkos::MDRangePolicy< Kokkos::Rank<2> > mdrange_policy;
+// Allocate arrays
+ViewMatrixType A("A", N, N);
+ViewMatrixType B("B", N, N);
+ViewMatrixType C("C", N, N);
+// More definitions
+float clocktime, err;
+
+// Fill values
+Kokkos::parallel_for( "initA", mdrange_policy({0,0}, {N,N}), KOKKOS_LAMBDA ( const int i , const int j ) {
+    A(i,j) = AVAL;
+  }
+);
+Kokkos::parallel_for( "initB", mdrange_policy({0,0}, {N,N}), KOKKOS_LAMBDA ( const int i , const int j ) {
+    B(i,j) = BVAL;
+  }
+);
+Kokkos::parallel_for( "initC", mdrange_policy({0,0}, {N,N}), KOKKOS_LAMBDA ( const int i , const int j ) {
+    C(i,j) = 0.;
+  }
+);
+
+// Start timer
+Kokkos::Timer timer;
+
+// MATMUL
+Kokkos::parallel_for( "matmul", mdrange_policy({0,0}, {N,N}), KOKKOS_LAMBDA ( const int i , const int j ) {
+    double sum = 0.;
+
+    for ( int k = 0; k < N; k++ ) {
+      double a = A(i,k);
+      double b = B(k,j);
+      sum += a * b;
+    }
+
+    C(i,j) = (float)sum;
+  }
+);
+Kokkos::fence();
+
+// Stop timer
+clocktime = (float)timer.seconds();
+
+// MATMUL verification
+err = 0.;
+Kokkos::parallel_reduce( "verify_matmul", mdrange_policy({0,0}, {N,N}), 
+  KOKKOS_LAMBDA ( const int i , const int j, float& tmperr ) {
+    tmperr += fabs( C(i,j) - tot );
+}, 
+err);
+
+// Print stuff
+cout << "N: " << N << "; ";
+cout << "Err: " << err << "; ";
+cout << "Clock[ms]: " << clocktime*1000. << "; ";
+cout << endl;
+
+}
+Kokkos::finalize();
+return 0;
+}

535f14f2739225435d5d151b529fb53835b8b71d
Marco De La Pierre <marco.delapierre@gmail.com>
2023-02-03 20:31:16 +0800

    added install kokkos kernels for topaz cpu, fixes for gpu
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

--- misc/install-kokkos-kernels-cpu.sh
+++ misc/install-kokkos-kernels-cpu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -l
 #SBATCH --account=pawsey0001
 #SBATCH --partition=gpuq-dev
-#SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=00:20:00
@@ -12,15 +11,14 @@
 download_kernels="0"
 kokkos_version="3.5.00"
 
-module load cuda/11.4.2
 module load cmake/3.18.0
 
 if [ "$download_kernels" != "0" ] ; then
- rm -rf kokkos-kernels
- git clone git@github.com:kokkos/kokkos-kernels
+ rm -rf kokkos-kernels-cpu
+ git clone git@github.com:kokkos/kokkos-kernels kokkos-kernels-cpu
 fi
 
-cd kokkos-kernels
+cd kokkos-kernels-cpu
 git checkout $kokkos_version
 
 mkdir build
@@ -29,8 +27,8 @@ cd build
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$(pwd)/../apps \
   -DCMAKE_CXX_COMPILER=g++ \
-  -DCMAKE_PREFIX_PATH=$(pwd)/../../kokkos/apps/lib64/cmake/Kokkos \
-  -DKokkos_ROOT=$(pwd)/../../kokkos/apps
+  -DCMAKE_PREFIX_PATH=$(pwd)/../../kokkos-cpu/apps/lib64/cmake/Kokkos \
+  -DKokkos_ROOT=$(pwd)/../../kokkos-cpu/apps
 
 make -j $SLURM_CPUS_PER_TASK
 sg pawsey0001 -c 'make install'

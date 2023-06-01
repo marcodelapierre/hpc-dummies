bd032840bb5c75bd7dffad5819c4734c484d7662
Marco De La Pierre <marco.delapierre@gmail.com>
2023-06-01 23:10:35 +0800

    utils/ jobstep examples from affinity tuto at cug
    Signed-off-by: Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ utils/jobstep-single.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=jobstep-single
+#SBATCH --output=%x.out
+#SBATCH --account=pawsey0012-gpu
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+#SBATCH --gpus-per-node=8
+#SBATCH --exclusive
+#SBATCH --time=00:01:00
+#SBATCH --export=NONE 
+
+module load rocm/5.0.2
+module load craype-accel-amd-gfx90a
+
+for n in {1..8} ; do srun --exact -n $n --gpus-per-node=$n --gpu-bind=single:1  ./hello_jobstep_hip.x ; echo " " ; done

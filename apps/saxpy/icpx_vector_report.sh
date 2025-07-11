8e3710a7dde9b26e4f29530af2d2974d9e91dd7d
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-07-11 14:16:14 +0800

    saxpy: adding vec report for -fnovec
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- apps/saxpy/icpx_vector_report.sh
+++ apps/saxpy/icpx_vector_report.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# serial with avx2 arch
-CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=core-avx2  -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp 
-mv saxpy_serial.optrpt saxpy_serial.optrpt_avx2
+# serial with vectorisation off
+CPATH=$(pwd)/../include:$CPATH icpx -O3 -fno-vectorize -fno-slp-vectorize  -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp
+mv saxpy_serial.optrpt saxpy_serial.optrpt_novec
 
 # serial with no arch (avx default)
 CPATH=$(pwd)/../include:$CPATH icpx -O3   -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp 
 mv saxpy_serial.optrpt saxpy_serial.optrpt_std
 
+# serial with avx2 arch
+CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=core-avx2  -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp 
+mv saxpy_serial.optrpt saxpy_serial.optrpt_avx2
+
+
 # avx2 intrinsics
 CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=core-avx2  -qopt-report=3 -qopt-report-phase=vec -o saxpy_avx2.x saxpy_avx2.cpp 
+
 
 # avx512 intrinsics
 CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=common-avx512  -qopt-report=3 -qopt-report-phase=vec -o saxpy_avx512.x saxpy_avx512.cpp 

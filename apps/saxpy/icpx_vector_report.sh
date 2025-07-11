7ec67ed46987846c89614c055cd72a4eb09a2168
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2025-07-11 14:14:03 +0800

    saxpy: adding avx512 code, plus optreport examples
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

---
+++ apps/saxpy/icpx_vector_report.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# serial with avx2 arch
+CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=core-avx2  -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp 
+mv saxpy_serial.optrpt saxpy_serial.optrpt_avx2
+
+# serial with no arch (avx default)
+CPATH=$(pwd)/../include:$CPATH icpx -O3   -qopt-report=3 -qopt-report-phase=vec -o saxpy_serial.x saxpy_serial.cpp 
+mv saxpy_serial.optrpt saxpy_serial.optrpt_std
+
+# avx2 intrinsics
+CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=core-avx2  -qopt-report=3 -qopt-report-phase=vec -o saxpy_avx2.x saxpy_avx2.cpp 
+
+# avx512 intrinsics
+CPATH=$(pwd)/../include:$CPATH icpx -O3 -march=common-avx512  -qopt-report=3 -qopt-report-phase=vec -o saxpy_avx512.x saxpy_avx512.cpp 
+
---
+++ apps/saxpy/saxpy_avx512.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <immintrin.h>
+#include "my_timer.h"
+
+using namespace std;
+
+// AVX512 SIMD size for floats
+#define SIMD 16
+
+
+// Verify SAXPY result
+float verify_saxpy( const float tot, const size_t n, const float* const y )
+{
+  float err = 0.;
+  for (size_t i = 0; i < n; i++) {
+    err += fabs( y[i] - tot );
+  }
+
+  return err;
+}
+
+// Perform SAXPY
+void saxpy( const size_t n, const float a, 
+            const float* const x, float* const y )
+{
+  __m512 veca = _mm512_set1_ps(a);
+
+  for ( size_t i = 0; i < n; i+=SIMD ) {
+    __m512 vecx = _mm512_load_ps( &x[i] );
+    __m512 vecy = _mm512_load_ps( &y[i] );
+    __m512 vecz = _mm512_fmadd_ps( veca, vecx, vecy );
+    _mm512_store_ps( &y[i], vecz );
+  }
+}
+
+
+int main( int argc, char** argv ) {
+
+// Size of problem
+// NOTE: this specific sizes divides by 16 (SIMD)
+const unsigned N = (1 << 26);
+// Random values
+const float XVAL = rand() % 1000000;
+const float YVAL = rand() % 1000000;
+const float AVAL = rand() % 1000000;
+const float tot = AVAL * XVAL + YVAL;
+// Allocate arrays
+float* x = (float*)aligned_alloc(32, N * sizeof(float));
+float* y = (float*)aligned_alloc(32, N * sizeof(float));
+// More definitions
+float clocktime, err;
+
+// Fill values
+for ( size_t i = 0; i < N; i++ ) {
+  x[i]   = XVAL;
+  y[i]   = YVAL;
+}
+
+// Start timer
+//clock_t start = clock();
+my_timer timer;
+// SAXPY
+saxpy( N, AVAL, x, y );
+// Stop timer
+//clock_t watch = clock() - start;
+//const float clocktime = ((float)watch)/CLOCKS_PER_SEC;
+clocktime = (float)timer.elapsed();
+
+// SAXPY verification
+err = verify_saxpy( tot, N, y );
+
+// Print stuff
+cout << "N: " << N << "; ";
+cout << "Err: " << err << "; ";
+cout << "Clock[ms]: " << clocktime*1000. << "; ";
+cout << endl;
+
+// Deallocate arrays
+delete [] y;
+delete [] x;
+
+return 0;
+}
